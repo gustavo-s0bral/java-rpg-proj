a431b3b698d4557b5c8f7f585775cd1d000e7813
Gustavo Sobral <gustavosobral1711@gmail.com>
2025-03-27 16:37:35 -0300

    Teste de menu do jogo

---
+++ testeTexto.java
@@ -0,0 +1,7 @@
+public class testeTexto {
+    public static void main(String[] args) {
+      String menu = "___________________________\n||         MENU          ||\n|| inventario     mapa   ||\n|| missões        xxxx   ||\n---------------------------";
+
+      System.out.println(menu);
+    }
+  }
