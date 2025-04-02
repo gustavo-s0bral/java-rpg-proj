cc6bb5dfb6d91ddd2ab4a3d45240378201661230
Gustavo Sobral <gustavosobral1711@gmail.com>
2025-04-02 09:12:45 -0300

    deixa pronto o sistema de xp

--- Xp.java
+++ Xp.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 
 public class Xp {
     public static void main(String[] args) {
-        int XpPlayer = 0;
+        int XpPlayer = 25;
         int pontosDeStatus = 0;
         int levelPlayer = 1;
+        
         int forcaStatus = 1;
-        int energiaStatus = 1;
-        int vidaStatus = 1;
-        int criticoStatus = 1;
-        int vidaStatusAdicional = 0;
         int forcaStatusAdicao = 0;
-        int lifeJogador = 100 + vidaStatusAdicional;
+
+        int energiaStatus = 1;
+        int energiaStatusAdicao = 0;
+
+        int vidaStatus = 1;
+        int vidaStatusAdicao = 0;
+
+        int criticoStatus = 1;
+        int criticoStatusAdicao = 0;
+
+        int lifeJogador = 100;
 
         if (XpPlayer == 25) {
             levelPlayer += 1;
@@ -21,11 +28,24 @@ public class Xp {
             int escolhaStatus = scanner1.nextInt();
             if (escolhaStatus == 1) {
                 System.out.println("Voce upou sua força, voce ganhou uma adicao de 5 DMG");
-                
+                forcaStatusAdicao += 5;
+                forcaStatus += 1;
+            }
+            else if(escolhaStatus == 2){
+                System.out.println("Você upou sua energia, ganhou uma adicao de +1 em chance de acertar");
+                energiaStatus += 1;
+                energiaStatusAdicao+=1;
+            }
+            else if (escolhaStatus == 3) {
+                System.out.println("Voce upou sua vida, ganhou +10 de HP.");
+                vidaStatus += 1;
+                vidaStatusAdicao += 10;
+                lifeJogador += vidaStatusAdicao;
             }
             scanner1.close();
         }
         System.out.println(levelPlayer);
         System.out.println(pontosDeStatus);
+        System.out.println(lifeJogador);
     }
 }
