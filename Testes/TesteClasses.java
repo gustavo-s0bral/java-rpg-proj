d59507e92e297deff6c3ec2a6a8bf8ba3cade225
Gustavo Sobral <gustavosobral1711@gmail.com>
2025-04-07 15:17:03 -0300

    Adiciona o inicio da classe Armas

--- Testes/TesteClasses.java
+++ Testes/TesteClasses.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
-public class Jogador {
+public class Armas{
+    String nomeArma;
+    int danoArma, danoArmaEspecial;
+
+    public static final Armas armaDeFogo = new Armas("Arma de fogo", 30, 80);
+    public static final Armas faca = new Armas("Faca", 10, 15);
+    public Armas(String nomeArma, int danoArma, int danoArmaEspecial){
+        this.nomeArma = nomeArma;
+        this.danoArma = danoArma;
+        this.danoArmaEspecial = danoArmaEspecial;
+    }
+}
+public class Jogador{
     String nome;
     int nivel = 1;
     int vida = 100;
@@ -17,6 +29,7 @@ public class Jogador {
 }
 public class TesteClasses {
     public static void main(String[] args) {
+        Armas armaEscolhida;
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Digite seu nome: ");
         String nomeJogador = scanner1.nextLine();
@@ -24,5 +37,11 @@ public class TesteClasses {
         Jogador jogador1 = new Jogador(nomeJogador);
         // Chamando um método do objeto
         jogador1.exibirStatus();
+
+        armaEscolhida = Armas.armaDeFogo;
+
+        System.out.println(armaEscolhida.nomeArma);
+        System.out.println(armaEscolhida.danoArma);
+        System.out.println(armaEscolhida.danoArmaEspecial);
     }
 }
